36fb545fff864d3922a3470e0bcc3143adbd3744
Lui Crowie <crowzfx@outlook.com>
2021-03-09 19:40:07 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,3 +9,6 @@ INSTALLSD_DIR=$GITHUB_DIR/installsd.git
 
 mkdir -p $GITHUB_DIR
 [ ! -d "$INSTALLSD_DIR" ]  && git clone https://github.com/crowz-fx/installsd.git $INSTALLSD_DIR || echo "Repo exists, skipping clone!"
+cd $INSTALLSD_DIR
+chmod 744 $INSTALLSD_DIR/Makefile $INSTALLSD_DIR/makefile.sh
+echo "Ready to run your make commands..."
