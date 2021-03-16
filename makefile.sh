940de460238e35f31caf6faeef2abe771b0c6473
Lui Crowie <crowzfx@outlook.com>
2021-03-16 11:05:24 +0000

    fixed indentation and added gnupg

--- makefile.sh
+++ makefile.sh
@@ -16,12 +16,12 @@ function install_zsh_config() {
     mkdir -p $HOME/scripts
     echo "PATH BEFORE"
     echo $PATH
-	echo "PATH=$PATH:$HOME/bin:$HOME/scripts" >> $HOME/.zshrc
+    echo "PATH=$PATH:$HOME/bin:$HOME/scripts" >> $HOME/.zshrc
     echo ""
     echo "PATH AFTER"
     echo $PATH
-	echo "export SDKMAN_DIR='$HOME/.sdkman'" >> $HOME/.zshrc
-	echo "[[ -s '$HOME/.sdkman/bin/sdkman-init.sh' ]] && source '$HOME/.sdkman/bin/sdkman-init.sh'" >> $HOME/.zshrc
+    echo "export SDKMAN_DIR='$HOME/.sdkman'" >> $HOME/.zshrc
+    echo "[[ -s '$HOME/.sdkman/bin/sdkman-init.sh' ]] && source '$HOME/.sdkman/bin/sdkman-init.sh'" >> $HOME/.zshrc
 }
 
 function install_pip_list() {
@@ -35,17 +35,17 @@ function install_pip_list() {
 function install_aws_kubectl_aws_iam_authentication() {
     echo "Install kubectl and AWS auth..."
     INSTALL_DIR=build
-	mkdir -p $INSTALL_DIR
+    mkdir -p $INSTALL_DIR
     mkdir -p $HOME/bin
-	curl -o $INSTALL_DIR/kubectl https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.3/2018-07-26/bin/darwin/amd64/kubectl
-	curl -o $INSTALL_DIR/kubectl.sha256 https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.3/2018-07-26/bin/darwin/amd64/kubectl.sha256
-	chmod +x $INSTALL_DIR/kubectl
-	$INSTALL_DIR/kubectl version --short --client
-	curl -o $INSTALL_DIR/aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.3/2018-07-26/bin/darwin/amd64/aws-iam-authenticator
-	curl -o $INSTALL_DIR/aws-iam-authenticator.sha256 https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.3/2018-07-26/bin/darwin/amd64/aws-iam-authenticator.sha256
-	chmod +x $INSTALL_DIR/aws-iam-authenticator
-	mv $INSTALL_DIR/kubectl $HOME/bin/
-	mv $INSTALL_DIR/aws-iam-authenticator $HOME/bin/
+    curl -o $INSTALL_DIR/kubectl https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.3/2018-07-26/bin/darwin/amd64/kubectl
+    curl -o $INSTALL_DIR/kubectl.sha256 https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.3/2018-07-26/bin/darwin/amd64/kubectl.sha256
+    chmod +x $INSTALL_DIR/kubectl
+    $INSTALL_DIR/kubectl version --short --client
+    curl -o $INSTALL_DIR/aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.3/2018-07-26/bin/darwin/amd64/aws-iam-authenticator
+    curl -o $INSTALL_DIR/aws-iam-authenticator.sha256 https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.3/2018-07-26/bin/darwin/amd64/aws-iam-authenticator.sha256
+    chmod +x $INSTALL_DIR/aws-iam-authenticator
+    mv $INSTALL_DIR/kubectl $HOME/bin/
+    mv $INSTALL_DIR/aws-iam-authenticator $HOME/bin/
     echo "Installed kubectl and AWS auth!"
 }
 
@@ -69,39 +69,39 @@ function install_sdkman_list(){
 }
 
 function install_brew_item() {
-	brew install $* || error_handler
+    brew install $* || error_handler
 }
 
 function install_brew_cask_item() {
-	brew install cask $* || error_handler
+    brew install cask $* || error_handler
 }
 
 function install_brew_list() {
-	echo "Installing brew..."
-	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-	echo "Installed brew!"
+    echo "Installing brew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+    echo "Installed brew!"
 
-	echo "Updating brew..."
-	brew update
+    echo "Updating brew..."
+    brew update
 
-	echo "Installing standard brew items..."
+    echo "Installing standard brew items..."
     BREW_INSTALL_ITEMS=( "colordiff" "fasd" "git" "git-extras" "git-flow" "htop-osx" "jq" "tmux" "tree" "wget" "zsh" "zsh-completions" "node" "ack" "fzf" \
         "python python3 pipenv" "watch" "yarn" "golang dep" "reattach-to-user-namespace" "the_silver_searcher"  "warrensbox/tap/tfswitch" \
-		"kubectx" "subversion@1.8" "ctop" "git-secret" "helm@2" "awscli" )
+		"kubectx" "subversion@1.8" "ctop" "git-secret" "helm@2" "awscli" "gnupg")
 	# "grv" "cheat" "ctags" "httpie" "pwgen" "pstree" "packer" "blueutil" "Graphviz" "node@8" "unrar"
 	
-	for item in "${BREW_INSTALL_ITEMS[@]}"
-	do
+    for item in "${BREW_INSTALL_ITEMS[@]}"
+    do
         install_brew_item $item
     done
     echo "Installed standard brew items!"
 
-	echo "Installing brew cask items..."
-	BREW_CASK_INSTALL_ITEMS=( "spectacle" "iterm2" "aws-vault" )
+    echo "Installing brew cask items..."
+    BREW_CASK_INSTALL_ITEMS=( "spectacle" "iterm2" "aws-vault" )
     # "google-chrome", "dropbox", "sourcetree", "postman", "virtualbox", "vagrant", "vagrant-manager" "visual-studio-code"
 
     for item in "${BREW_CASK_INSTALL_ITEMS[@]}"
-	do
+    do
         install_brew_cask_item $item
     done
     echo "Installed brew cask items!"
